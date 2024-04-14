d3f6e9a5b3fccda35cfaed06aa941c59cc4f38f9
willslipknot <62850730+willslipknot@users.noreply.github.com>
2024-04-13 20:08:46 -0600

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    //baseURL:'http://localhost:3001',
-    baseURL: 'https://raicesruralesback-production.up.railway.app',
+    baseURL:'http://localhost:3001',
     withCredentials:true,
     
 })
