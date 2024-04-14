2097236f513616b86acb6387439d124ce780152d
Willslipknot <willslipknotpsico@gmail.com>
2024-04-13 21:26:53 -0600

    aw

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,4 +1,4 @@
-import axios from './axios.js';
+/*import axios from './axios.js';
 
 export const registerRequest = user => axios.post(`/register`, user)
 
@@ -6,4 +6,49 @@ export const loginRequest = user => axios.post(`/login`, user)
 
 export const verifyTokenRequest = () => axios.get(`/verify`)
 
-export const updateUserRequest = (id, user) => axios.put(`/profile/${id}`, user)
+export const updateUserRequest = (id, user) => axios.put(`/profile/${id}`, user)*/
+
+
+// api/auth.js
+
+import axios from './axios.js';
+
+export const registerRequest = async (user) => {
+  try {
+    const res = await axios.post('/api/register', user);
+    return res.data;
+  } catch (error) {
+    throw error.response;
+  }
+};
+
+export const loginRequest = async (user) => {
+  try {
+    const res = await axios.post('/api/login', user);
+    return res.data;
+  } catch (error) {
+    throw error.response;
+  }
+};
+
+export const verifyTokenRequest = async (token) => {
+  try {
+    const res = await axios.get('/api/verifyToken', {
+      headers: {
+        Authorization: token,
+      },
+    });
+    return res.data;
+  } catch (error) {
+    throw error.response;
+  }
+};
+
+export const updateUserRequest = async (id, user) => {
+  try {
+    const res = await axios.put(`/api/users/${id}`, user);
+    return res.data;
+  } catch (error) {
+    throw error.response;
+  }
+};
