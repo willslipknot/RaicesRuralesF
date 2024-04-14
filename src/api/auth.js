2b5755b03a9ac8000538c23887da7509137d8ced
Willslipknot <willslipknotpsico@gmail.com>
2024-04-13 21:25:07 -0600

    aq

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,42 +1,9 @@
+import axios from './axios.js';
 
-import axios from 'axios';
+export const registerRequest = user => axios.post(`/register`, user)
 
-export const registerRequest = async (user) => {
-  try {
-    const res = await axios.post('/register', user);
-    return res.data;
-  } catch (error) {
-    throw error.response;
-  }
-};
+export const loginRequest = user => axios.post(`/login`, user)
 
-export const loginRequest = async (user) => {
-  try {
-    const res = await axios.post('/login', user);
-    return res.data;
-  } catch (error) {
-    throw error.response;
-  }
-};
+export const verifyTokenRequest = () => axios.get(`/verify`)
 
-export const verifyTokenRequest = async (token) => {
-  try {
-    const res = await axios.get('/verifyToken', {
-      headers: {
-        Authorization: token,
-      },
-    });
-    return res.data;
-  } catch (error) {
-    throw error.response;
-  }
-};
-
-export const updateUserRequest = async (id, user) => {
-  try {
-    const res = await axios.put(`/users/${id}`, user);
-    return res.data;
-  } catch (error) {
-    throw error.response;
-  }
-};
+export const updateUserRequest = (id, user) => axios.put(`/profile/${id}`, user)
